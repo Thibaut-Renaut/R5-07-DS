6bb152e1d480e746d0b300959c512676f66377fa
Thibaut-Renaut <130363631+Thibaut-Renaut@users.noreply.github.com>
2024-11-20 13:56:47 +0100

    Create Q3.java
    création du fichier q3.java

---
+++ Q3.java
@@ -0,0 +1,8 @@
+public class Q3 {
+    public static void main(String[] args) {
+        int stars = Integer.parseInt(args[0]);
+        for (int i = 0; i < stars; i++) {
+            System.out.print("*");
+        }
+    }
+}
